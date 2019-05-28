1a634404f0638a76ba4560ec2de06fc7c8b5364d
serg1122 <serg@nutshell.ru>
2019-05-28 20:54:21 +0300

    Matshalling/Unmarshalling for OptionalFloat64

--- immutable/float64.go
+++ immutable/float64.go
@@ -1,6 +1,10 @@
 package immutable
 
-import "github.com/serg1122/optional"
+import (
+	"encoding/json"
+
+	"github.com/serg1122/optional"
+)
 
 type OptionalFloat64 struct {
 	isPresent bool
@@ -30,5 +34,29 @@ func (o *OptionalFloat64) ValueSet(value float64) *optional.ErrorValueIsPresent 
 	}
 	o.value = value
 	o.isPresent = true
+	return nil
+}
+
+func (o *OptionalFloat64) MarshalJSON() ([]byte, error) {
+	if o.IsPresent() {
+		return json.Marshal(o.value)
+	}
+	return json.Marshal(nil)
+}
+
+func (o *OptionalFloat64) UnmarshalJSON(data []byte) error {
+	if o.IsPresent() {
+		return optional.ErrorValueIsPresentCreate()
+	}
+	if string(data) == "null" {
+		return nil
+	}
+	var value float64
+	if err1 := json.Unmarshal(data, &value); err1 != nil {
+		return err1
+	}
+	if err2 := o.ValueSet(value); err2 != nil {
+		return err2
+	}
 	return nil
 }
--- immutable/float64_test.go
+++ immutable/float64_test.go
@@ -1,6 +1,7 @@
 package immutable
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/serg1122/optional"
@@ -42,4 +43,40 @@ func TestOptionalFloat64_ValueSet(t *testing.T) {
 	assert.IsType(t, err3, optional.ErrorValueIsPresentCreate())
 	valueGot2, _ := opFloat64.ValueGet()
 	assert.Equal(t, valueGot2, valueExpected)
+}
+
+func TestOptionalFloat64_MarshalJSON(t *testing.T) {
+	opFloat64 := OptionalFloat64Create()
+
+	value1, err1 := opFloat64.MarshalJSON()
+	assert.Equal(t, []byte("null"), value1)
+	assert.Nil(t, err1)
+
+	opFloat64.ValueSet(float64(1.23))
+	value2, err2 := opFloat64.MarshalJSON()
+	assert.Equal(t, []byte("1.23"), value2)
+	assert.Nil(t, err2)
+}
+
+func TestOptionalFloat64_UnmarshalJSON(t *testing.T) {
+	opFloat64 := OptionalFloat64Create()
+
+	err1 := opFloat64.UnmarshalJSON([]byte("asd"))
+	assert.IsType(t, err1, &json.SyntaxError{})
+	assert.False(t, opFloat64.IsPresent())
+
+	err2 := opFloat64.UnmarshalJSON([]byte("null"))
+	assert.Nil(t, err2)
+	assert.False(t, opFloat64.IsPresent())
+
+	err3 := opFloat64.UnmarshalJSON([]byte("3.45"))
+	assert.Nil(t, err3)
+	assert.True(t, opFloat64.IsPresent())
+	valueGot1, _ := opFloat64.ValueGet()
+	assert.Equal(t, float64(3.45), valueGot1)
+
+	err4 := opFloat64.UnmarshalJSON([]byte("4.56"))
+	assert.IsType(t, err4, optional.ErrorValueIsPresentCreate())
+	valueGot2, _ := opFloat64.ValueGet()
+	assert.Equal(t, valueGot2, float64(3.45))
 }
