dbc2358fae620667048faf7f5acf0ec7e1facaf7
Никитин Сергей Олегович <s.nikitin@id-network.ru>
2019-05-29 15:34:05 +0300

    Marshalling/Unmarshallin for OptionalInt

--- immutable/int_test.go
+++ immutable/int_test.go
@@ -1,6 +1,7 @@
 package immutable
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/serg1122/optional"
@@ -39,4 +40,42 @@ func TestOptinoalInt_ValueSet(t *testing.T) {
 	assert.Equal(t, valueGot, valueExpected)
 	err3 := opInt.ValueSet(789)
 	assert.IsType(t, err3, optional.ErrorValueIsPresentCreate())
+}
+
+func TestOptinalInt_MarshalJSON(t *testing.T) {
+	opInt := OptionalIntCreate()
+
+	valueGot1, err1 := opInt.MarshalJSON()
+	assert.Equal(t, []byte("null"), valueGot1)
+	assert.Nil(t, err1)
+
+	opInt.ValueSet(int(5))
+	valueGot2, err2 := opInt.MarshalJSON()
+	assert.Equal(t, valueGot2, []byte("5"))
+	assert.Nil(t, err2)
+}
+
+func TestOptionalInt_UnmarshalJSON(t *testing.T) {
+	opInt := OptionalIntCreate()
+
+	err1 := opInt.UnmarshalJSON([]byte("qwe"))
+	assert.IsType(t, err1, &json.SyntaxError{})
+	assert.False(t, opInt.IsPresent())
+
+	err2 := opInt.UnmarshalJSON([]byte("null"))
+	assert.Nil(t, err2)
+	assert.False(t, opInt.IsPresent())
+
+	valueExpected := int(6)
+
+	err3 := opInt.UnmarshalJSON([]byte("6"))
+	valueGot1, _ := opInt.ValueGet()
+	assert.True(t, opInt.IsPresent())
+	assert.Equal(t, valueGot1, valueExpected)
+	assert.Nil(t, err3)
+
+	err4 := opInt.UnmarshalJSON([]byte("6"))
+	valueGot2, _ := opInt.ValueGet()
+	assert.Equal(t, err4, optional.ErrorValueIsPresentCreate())
+	assert.Equal(t, valueGot2, valueExpected)
 }
